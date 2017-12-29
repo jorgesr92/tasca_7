b01766fe5cc37d4c2892267feb30c78a4b3aa315
jorgesr92 <jr_6924@hotmail.com>
2017-12-29 10:54:49 +0100

    commint revision 4

---
+++ js/pedirfecha.js
@@ -0,0 +1,8 @@
+function pedirfecha(fecha) {
+    var fecha= { //pedimos la fecha
+        dia: prompt ("introduce un día"),
+        mes: prompt ("introduce un mes"),
+        año: prompt ("introduce un año entre 1700 y 2299")
+    }
+    return fecha;
+}
